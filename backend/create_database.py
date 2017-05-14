5108c6745f4421233c0153bfbbcd7e0f420e6e33
anil1596 <anil.khadwal@gmail.com>
2017-05-14 18:30:22 +0530

    updated server

--- backend/create_database.py
+++ backend/create_database.py
@@ -7,17 +7,6 @@ postgres = psycopg2.connect(database = 'web_portal', host = 'localhost', user = 
 cursor = postgres.cursor()
 
 cursor.execute("""
-   select exists(select 0 from pg_class where relname = 'item')
-""")
-
-presence = cursor.fetchone()[0]
-if presence:
-    print('item table exists')
-    cursor.execute("""
-        drop table item
-    """)
-
-cursor.execute("""
    select exists(select 0 from pg_class where relname = 'customer')
 """)
 
@@ -29,18 +18,19 @@ if presence:
         drop table customer
     """)
 
-
 cursor.execute("""
-   select exists(select 0 from pg_class where relname = 'seller')
+   select exists(select 0 from pg_class where relname = 'item')
 """)
-    
-presence = cursor.fetchone()[0]
 
+presence = cursor.fetchone()[0]
 if presence:
-    print('seller table exists')
+    print('item table exists')
     cursor.execute("""
-        drop table seller
+        drop table item
     """)
+
+
+
 
 
 try:
@@ -51,17 +41,22 @@ except Exception as e:
 
 
 
-#                       CREATE TABLE SELLER                                         #
+#                       CREATE TABLE ITEM                                        #
 cursor.execute("""
-    create table seller(
-        seller_id bigserial PRIMARY KEY,
-        first_name text NOT NULL,
-        last_name text NOT NULL,
-        email text NOT NULL,
-        mobile varchar(12)[] NOT NULL,
-        hostel text NOT NULL,
-        room text NOT NULL,
-        password text NOT NULL
+    create table item(
+        item_no bigserial  ,
+        name text ,
+        email text ,
+        mobile text ,
+        hostel text ,
+        room text ,
+        item_name text ,
+        item_type text ,
+        sold boolean ,
+        price text,
+        item_description text ,
+        imageaddress text,
+        PRIMARY KEY(item_no)
     )
 """)
 
@@ -80,10 +75,11 @@ cursor.execute("""
         first_name text NOT NULL,
         last_name text NOT NULL,
         email text NOT NULL,
-        mobile varchar(12)[] NOT NULL,
+        mobile text,
         hostel text NOT NULL,
         room text NOT NULL,
-        password text NOT NULL
+        bid_amount text NOT NULL,
+        item_id int NOT NULL references item(item_no)
     )
 """)
 
@@ -95,114 +91,22 @@ except Exception as e:
     print(e)
 
 
-#                       CREATE TABLE ITEMS                                      #
-cursor.execute("""
-    create table item(
-        seller_id int NOT NULL references seller(seller_id),
-        item_no bigserial  ,
-        item_name text NOT NULL,
-        item_type varchar(10) NOT NULL CHECK(item_type in ('starter','main','desert')) ,
-        sold boolean NOT NULL ,
-        price text NOT NULL,
-        item_description text NOT NULL,
-        imageaddress text,
-        discount double precision,
-        PRIMARY KEY(seller_id, item_no)
-    )
-""")
-#                       COMMIT AND ROLLBACK IF EXCEPTION                             #
-try:
-    postgres.commit()
-except Exception as e:
-    postgres.rollback()
-    print(e)
-
-
-
-# #                       CREATE TABLE CUSTOMERS                                            #
+# #                       CREATE TABLE ITEMS                                      #
 # cursor.execute("""
-#     create table customers(
-#             customer_id bigserial PRIMARY KEY NOT NULL,
-#             customer_name text NOT NULL,
-#             emailid text NOT NULL,
-#             mobile varchar(12)[] NOT NULL,
-#             address text,
-#             password text NOT NULL
-#     )
-# """)
-# try:
-#     postgres.commit()
-# except Exception as e:
-#     postgres.rollback()
-#     print(e)
-
-
-# #                               CHECK FOR TABLE FOODIES_RECORD                      #
-# # cursor.execute("""
-# #    select exists(select 0 from pg_class where relname = 'foodiesrecord')
-# # """)
-
-# # presence = cursor.fetchone()[0]
-# # print(presence)
-
-# #  #                       DELETE FOODIESRECORD IF EXISTS                              #
-# # if presence:
-# #     print('foodiesrecord table exists, deleting here')
-# #     cursor.execute("""
-# #         drop table foodiesrecord
-# #     """)
-
-# # #                       CREATE TABLE FOODIESRECORD                                     #
-# # cursor.execute("""
-# #     create table foodiesrecord(
-# #             transactionid bigserial PRIMARY KEY NOT NULL,
-# #             ordered_on timestamp NOT NULL,
-# #             customer_id int NOT NULL,
-# #             delivered_on timestamp,
-# #             vendorid int NOT NULL,
-# #             status varchar(20) NOT NULL,
-# #             bill money NOT NULL
-# #     )
-# # """)
-# # try:
-# #     postgres.commit()
-# # except Exception as e:
-# #     postgres.rollback()
-# #     print(e)
-
-
-
-# #                       CREATE TABLE ACCOUNTS_RECORD                                    #
-# cursor.execute("""
-#     create table accounts_record(
-#         v_id bigserial PRIMARY KEY NOT NULL references vendors(vendorid),
-#         account smallint NOT NULL
-#     )
-# """)
-# try:
-#     postgres.commit()
-# except Exception as e:
-#     postgres.rollback()
-#     print(e)
-
-
-
-
-# #                       CREATE TABLE ORDERsRECORD                                   #
-# cursor.execute("""
-#     create table ordersrecord(
-#         order_id int PRIMARY KEY,
-#         vendor_id int NOT NULL references vendors(vendorid),
-#         customer_id int NOT NULL references customers(customer_id),
-#         ordered_placed_on timestamp NOT NULL,
-#         ordered_deliverd_on timestamp ,
-#         order_status character(1) CHECK(order_status in('y','n')),
-#         description text NOT NULL,
-#         amount text NOT NULL
+#     create table item(
+#         seller_id int NOT NULL references seller(seller_id),
+#         item_no bigserial  ,
+#         item_name text NOT NULL,
+#         item_type varchar(10) NOT NULL CHECK(item_type in ('starter','main','desert')) ,
+#         sold boolean NOT NULL ,
+#         price text NOT NULL,
+#         item_description text NOT NULL,
+#         imageaddress text,
+#         discount double precision,
+#         PRIMARY KEY(seller_id, item_no)
 #     )
 # """)
 # #                       COMMIT AND ROLLBACK IF EXCEPTION                             #
-
 # try:
 #     postgres.commit()
 # except Exception as e:
--- backend/server.go
+++ backend/server.go
@@ -19,9 +19,9 @@ func init() {
 	db, db_err = pgx.NewConnPool(pgx.ConnPoolConfig{
 		ConnConfig: pgx.ConnConfig{
 			Host:     "localhost",
-			Database: "foodies",
+			Database: "web_portal",
 			User:     "anil",
-			Password: "205474",
+			Password: "anil205474",
 			Port:     5432,
 		},
 		MaxConnections: 10,
@@ -36,11 +36,11 @@ func main() {
 	r := gin.Default()
 
 	//*************************Hosting client.html page
-	r.GET("/main", func(c *gin.Context) {
-		res, _ := ioutil.ReadFile("/home/anil/foodies/spicyX/base/main.html")
+
+	r.GET("/uploaditem", func(c *gin.Context) {
+		res, _ := ioutil.ReadFile("/home/anil/major2/web/desi_carting/uploaditem.html")
 		c.Data(200, "text/html", res)
 	})
-
 	//**********************fetching Javascript files file
 	r.GET("/js/:js_file", func(c *gin.Context) {
 		//to ser
@@ -94,200 +94,207 @@ func main() {
 		// c.Data(200, path.Join("applications", "javascript"), res)
 	})
 
-	//********************Registering vendors
-	r.POST("/registervendor", func(c *gin.Context) {
-		var ven vendor
+	// //********************Registering vendors
+	// r.POST("/registervendor", func(c *gin.Context) {
+	// 	var ven vendor
 
-		c.BindJSON(&ven)
+	// 	c.BindJSON(&ven)
 
-		fmt.Println("\n\nRequest Received  for vendor registration: \n\n ")
+	// 	fmt.Println("\n\nRequest Received  for vendor registration: \n\n ")
 
-		tx, _ := db.Begin() // tx => transaction , _ => error and execute
+	// 	tx, _ := db.Begin() // tx => transaction , _ => error and execute
 
-		defer tx.Rollback() // it will be executed after the completion of local function
-		fmt.Println(ven.Owner, ven.Name, ven.Email, ven.Mobile, ven.Address, ven.Image, ven.Description, ven.Offer, ven.Password)
-		// var track ID
-		var num VID
-		// insert into users table
-		err := tx.QueryRow(`
-        INSERT INTO vendors (owner, vendorname, email ,mobile ,address  ,imageaddress ,description,offer, password ) values ($1, $2, $3, $4, $5, $6, $7,$8,$9) returning vendorid
-          `, ven.Owner, ven.Name, ven.Email, ven.Mobile, ven.Address, ven.Image, ven.Description, ven.Offer, ven.Password).Scan(&num.Vendorid)
-		fmt.Println(err)
-		commit_err := tx.Commit()
+	// 	defer tx.Rollback() // it will be executed after the completion of local function
+	// 	fmt.Println(ven.Owner, ven.Name, ven.Email, ven.Mobile, ven.Address, ven.Image, ven.Description, ven.Offer, ven.Password)
+	// 	// var track ID
+	// 	var num VID
+	// 	// insert into users table
+	// 	err := tx.QueryRow(`
+	//     INSERT INTO vendors (owner, vendorname, email ,mobile ,address  ,imageaddress ,description,offer, password ) values ($1, $2, $3, $4, $5, $6, $7,$8,$9) returning vendorid
+	//       `, ven.Owner, ven.Name, ven.Email, ven.Mobile, ven.Address, ven.Image, ven.Description, ven.Offer, ven.Password).Scan(&num.Vendorid)
+	// 	fmt.Println(err)
+	// 	commit_err := tx.Commit()
 
-		if commit_err != nil {
-			tx.Rollback()
-			c.JSON(500, "ERR")
-			return
-		}
-		fmt.Println("Vendor registered and his ID:", num)
-		c.JSON(200, num)
+	// 	if commit_err != nil {
+	// 		tx.Rollback()
+	// 		c.JSON(500, "ERR")
+	// 		return
+	// 	}
+	// 	fmt.Println("Vendor registered and his ID:", num)
+	// 	c.JSON(200, num)
 
-	})
+	// })
 
 	//I**************************tem menu updation
 	r.POST("/additems", func(c *gin.Context) {
 		var val item
 
+		fmt.Println("\n\nRequest Received for Adding items: \n\n ")
 		c.BindJSON(&val)
 
-		fmt.Println("\n\nRequest Received for menu updation: \n\n ")
 		// fmt.Printf("%#v", menu)
 		tx, _ := db.Begin() // tx => transaction , _ => error and execute
 
 		defer tx.Rollback() // it will be executed after the completion of local function
 
-		// fmt.Println("vals", val.Vendorid, val.Itemno, val.Name, val.IType, val.Nature, val.Price, val.Description, val.Image, val.Discount)
-		_, err := tx.Exec(`
-				INSERT INTO itemmenu (vendor_id ,item_name ,item_type ,item_nature ,price , item_description ,imageaddress ,discount) 
-				         values ($1,$2,$3,$4,$5,$6,$7,$8)`, val.Vendorid, val.Name, val.IType, val.Nature, val.Price,
-			val.Description, val.Image, val.Discount)
+		fmt.Println("vals:", val.Item_no, val.Name, val.Email, val.Mobile, val.Hostel, val.Room,
+			val.Itemname, val.Itemtype, val.Sold, val.Price, val.Itemdescription, val.Imageaddress)
+
+		// temp := "anil"
+		// _, err := tx.Exec(`INSERT INTO item (name ,email , mobile , hostel , room , item_name , item_type  , sold , price ,
+		// item_description, imageaddress  ) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) `, temp, temp, temp, temp, temp, temp, temp, false, temp, temp, temp)
+
+		var item_num int
+
+		err := tx.QueryRow(`INSERT INTO item (name ,email , mobile , hostel , room , item_name , item_type  , sold , price ,
+		item_description, imageaddress  ) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) returning item_no`, val.Name, val.Email, val.Mobile,
+			val.Hostel, val.Room, val.Itemname, val.Itemtype, false, val.Price, val.Itemdescription, val.Imageaddress).Scan(&item_num)
 
 		if err != nil {
 			// c.JSON(500, "error")
-			fmt.Println("error", err)
+			fmt.Println("error while adding \n\n", err)
 		}
 
 		commit_err := tx.Commit()
 
 		if commit_err != nil {
 			tx.Rollback()
+			fmt.Println("error while committing \n\n")
 			fmt.Println(commit_err)
 			c.JSON(500, "ERR")
 			return
 		}
-		// fmt.Println("Menu  updated")
 		c.JSON(200, 1)
 
 	})
 
 	//*************************customer registration
-	r.GET("/registercustomer", func(c *gin.Context) {
-		var cus customer
-		c.BindJSON(&cus)
+	// r.GET("/registercustomer", func(c *gin.Context) {
+	// 	var cus customer
+	// 	c.BindJSON(&cus)
 
-		fmt.Println("\n\nRequest Received : \n\n")
+	// 	fmt.Println("\n\nRequest Received : \n\n")
 
-		tx, _ := db.Begin() // tx => transaction , _ => error and execute
+	// 	tx, _ := db.Begin() // tx => transaction , _ => error and execute
 
-		defer tx.Rollback() // it will be executed after the completion of local function
+	// 	defer tx.Rollback() // it will be executed after the completion of local function
 
-		var track CSID
+	// 	// var track CSID
 
-		// insert into users table
-		tx.QueryRow(`
-        INSERT INTO customers (customer_name, emailid ,mobile ,address ,password ) values ($1, $2, $3, $4, $5) returning customer_id
-          `, cus.Name, cus.Email, cus.Mobile, cus.Address, cus.Password).Scan(&track.Customerid)
+	// 	// insert into users table
+	// 	tx.QueryRow(`
+	//     INSERT INTO customers ( customer_id,first_name, last_name, email , mobile , hostel , room ,   bid_amount text NOT NULL,
+	//     item_id int NOT NULL references item(item_no) ) values ($1, $2, $3, $4, $5) returning customer_id
+	//       `, cus.Name, cus.Email, cus.Mobile, cus.Address, cus.Password).Scan(&track.Customerid)
 
-		commit_err := tx.Commit()
+	// 	commit_err := tx.Commit()
 
-		if commit_err != nil {
-			tx.Rollback()
-			c.JSON(500, "ERR")
-			return
-		}
-		fmt.Println("cutomer registered and his ID:", track.Customerid)
-		c.JSON(200, track)
+	// 	if commit_err != nil {
+	// 		tx.Rollback()
+	// 		c.JSON(500, "ERR")
+	// 		return
+	// 	}
+	// 	// fmt.Println("cutomer registered and his ID:", track.Customerid)
+	// 	c.JSON(200, track)
 
-	})
+	// })
 
-	//*****************************Serving vendors and their id's
-	r.GET("/getvendors", func(c *gin.Context) {
-		// c.BindJSON(&cus)
+	// //*****************************Serving vendors and their id's
+	// r.GET("/getvendors", func(c *gin.Context) {
+	// 	// c.BindJSON(&cus)
 
-		fmt.Println("\n\nRequest Received : \n\n")
+	// 	fmt.Println("\n\nRequest Received : \n\n")
 
-		rows, err := db.Query(` SELECT  vendorid, vendorname from vendors `)
-		if err != nil {
-			fmt.Println(err)
-			c.JSON(500, "error while retreiving vendors data")
-		}
+	// 	rows, err := db.Query(` SELECT  vendorid, vendorname from vendors `)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		c.JSON(500, "error while retreiving vendors data")
+	// 	}
 
-		defer rows.Close()
+	// 	defer rows.Close()
 
-		// var vendors = make(map[string]int)
-		ven := make([]VendorsToSend, 0)
+	// 	// var vendors = make(map[string]int)
+	// 	ven := make([]VendorsToSend, 0)
 
-		for rows.Next() {
-			var t VendorsToSend
-			err := rows.Scan(&t.Vendorid, &t.Vendorname)
-			ven = append(ven, t)
-			if err != nil {
-				fmt.Println(err)
-				c.JSON(500, "error while retreiving vendors data")
-			}
-		}
-		c.JSON(200, ven)
-		fmt.Println("Vendors names are sent")
-	})
+	// 	for rows.Next() {
+	// 		var t VendorsToSend
+	// 		err := rows.Scan(&t.Vendorid, &t.Vendorname)
+	// 		ven = append(ven, t)
+	// 		if err != nil {
+	// 			fmt.Println(err)
+	// 			c.JSON(500, "error while retreiving vendors data")
+	// 		}
+	// 	}
+	// 	c.JSON(200, ven)
+	// 	fmt.Println("Vendors names are sent")
+	// })
 
-	//****************** method to serve request for MENU of particular vendor
-	r.POST("/getvendorsmenu", func(c *gin.Context) {
-		var id VID
-		c.BindJSON(&id)
+	// //****************** method to serve request for MENU of particular vendor
+	// r.POST("/getvendorsmenu", func(c *gin.Context) {
+	// 	var id VID
+	// 	c.BindJSON(&id)
 
-		fmt.Println("\n\nRequest for retreiving vendors menu Received : \n\n")
+	// 	fmt.Println("\n\nRequest for retreiving vendors menu Received : \n\n")
 
-		rows, err := db.Query(` SELECT  item_no, item_name, item_type, item_nature, price, item_description, imageaddress, discount
-		                        from itemmenu where vendor_id = $1 `, id.Vendorid)
+	// 	rows, err := db.Query(` SELECT  item_no, item_name, item_type, item_nature, price, item_description, imageaddress, discount
+	// 	                        from itemmenu where vendor_id = $1 `, id.Vendorid)
 
-		if err != nil {
-			fmt.Println(err)
-			c.JSON(500, "error while retreiving vendors menu")
-		}
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		c.JSON(500, "error while retreiving vendors menu")
+	// 	}
 
-		defer rows.Close()
+	// 	defer rows.Close()
 
-		// var vendors = make(map[string]int)
-		items := make([]item, 0)
+	// 	// var vendors = make(map[string]int)
+	// 	items := make([]item, 0)
 
-		for rows.Next() {
-			var t item
-			err := rows.Scan(&t.Itemno, &t.Name, &t.IType, &t.Nature, &t.Price, &t.Description, &t.Image, &t.Discount)
-			items = append(items, t)
-			if err != nil {
-				fmt.Println(err)
-				c.JSON(500, "error while retreiving vendors menu")
-			}
-		}
-		c.JSON(200, items)
-		fmt.Println("Vendors Menu  sent")
-	})
+	// 	for rows.Next() {
+	// 		var t item
+	// 		err := rows.Scan(&t.Itemno, &t.Name, &t.IType, &t.Nature, &t.Price, &t.Description, &t.Image, &t.Discount)
+	// 		items = append(items, t)
+	// 		if err != nil {
+	// 			fmt.Println(err)
+	// 			c.JSON(500, "error while retreiving vendors menu")
+	// 		}
+	// 	}
+	// 	c.JSON(200, items)
+	// 	fmt.Println("Vendors Menu  sent")
+	// })
 
 	fmt.Println("\n\n\t #####     Foodies server live on :7070     #####")
 	r.Run(":7070")
 }
 
 // vendor holds the incoming requests for a vendor registration.
-type vendor struct {
-	Vendorid    int      `json:"vendorid,omitempty"`
-	Owner       string   `json:"owner"`
-	Name        string   `json:"vendorname"`
-	Email       string   `json:"email"`
-	Mobile      []string `json:"mobile"`
-	Address     string   `json:"address"`
-	Image       string   `json:"imageaddress,omitempty"`
-	Description string   `json:"description,omitempty"`
-	Offer       string   `json:"offer,omitempty"`
-	Password    string   `json:"password"`
+type customer struct {
+	Customer_id int    `json:"customer_id,omitempty"`
+	First_name  string `json:"first_name"`
+	Last_name   string `json:"last_name"`
+	Email       string `json:"email"`
+	Mobile      string `json:"mobile"`
+	Hostel      string `json:"hostel"`
+	Room        string `json:"room"`
+	Bid_amount  string `json:"bid_amount"`
+	Item_id     int    `json:"item_id"`
 }
 
 type VID struct {
 	Vendorid int `json:"vendorid"`
 }
 
-type customer struct {
-	Customerid int      `json:"customer_id,omitempty"`
-	Name       string   `json:"customer_name"`
-	Email      string   `json:"emailid"`
-	Mobile     []string `json:"mobile"`
-	Address    string   `json:"address"`
-	Password   string   `json:"password"`
-}
+// type customer struct {
+// 	Customerid int      `json:"customer_id,omitempty"`
+// 	Name       string   `json:"customer_name"`
+// 	Email      string   `json:"emailid"`
+// 	Mobile     []string `json:"mobile"`
+// 	Address    string   `json:"address"`
+// 	Password   string   `json:"password"`
+// }
 
-type CSID struct {
-	Customerid int `json:"customerid,omitempty"`
-}
+// type CSID struct {
+// 	Customerid int `json:"customerid,omitempty"`
+// }
 
 // type MENU struct {
 // 	ITEMS []item `json:"items"`
@@ -295,18 +302,34 @@ type CSID struct {
 
 //Menu updation
 type item struct {
-	Vendorid    int     `json:"vendor_id"`
-	Itemno      int     `json:"item_no,omitempty"`
-	Name        string  `json:"item_name"`
-	IType       string  `json:"item_type"`
-	Nature      bool    `json:"item_nature"`
-	Description string  `json:"item_description"`
-	Price       string  `json:"price"`
-	Image       string  `json:"imageaddress,omitempty"`
-	Discount    float64 `json:"discount,omitempty"`
+	Item_no         int    `json:"item_no,omitempty"`
+	Name            string `json:"name"`
+	Email           string `json:"email"`
+	Mobile          string `json:"mobile"`
+	Hostel          string `json:"hostel"`
+	Room            string `json:"room"`
+	Itemname        string `json:"itemname"`
+	Itemtype        string `json:"itemtype"`
+	Sold            string `json:"sold"`
+	Price           string `json:"price"`
+	Itemdescription string `json:"itemdescription"`
+	Imageaddress    string `json:"imageaddress,omitempty"`
 }
 
-type VendorsToSend struct {
-	Vendorid   int    `json:"vendor_id"`
-	Vendorname string `json:"vendorname"`
-}
+// //Menu updation
+// type item struct {
+// 	Vendorid    int     `json:"vendor_id"`
+// 	Itemno      int     `json:"item_no,omitempty"`
+// 	Name        string  `json:"item_name"`
+// 	IType       string  `json:"item_type"`
+// 	Nature      bool    `json:"item_nature"`
+// 	Description string  `json:"item_description"`
+// 	Price       string  `json:"price"`
+// 	Image       string  `json:"imageaddress,omitempty"`
+// 	Discount    float64 `json:"discount,omitempty"`
+// }
+
+// type VendorsToSend struct {
+// 	Vendorid   int    `json:"vendor_id"`
+// 	Vendorname string `json:"vendorname"`
+// }
